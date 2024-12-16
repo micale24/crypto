bcf568a3aaf148d6575edfab872527997a66b288
micale24 <kaihall.usaf@gmail.com>
2024-12-16 18:36:59 -0500

    started contract 1

---
+++ Solidity/welcome_toSol.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract WelcomToSolidity {
+    constructor() public{
+    }
+    function getResult() public view returns(uint){
+        uint a = 1;
+        uint b = 2;
+        uint result = a + b;
+        return result;
+    }
+}
